b2e29472596306476c7225faf7ca533f3ddf0dec
Dinh Tuans <dinhmanhtuans89@gmail.com>
2024-01-20 21:58:31 +0700

    xem mặt hàng đã đặt

--- BookStore/Controllers/OrderController.cs
+++ BookStore/Controllers/OrderController.cs
@@ -3,6 +3,7 @@ using BookStore.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +25,69 @@ namespace BookStore.Controllers
             _cart = cart;
         }
         public IActionResult Checkout()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetOrder()
+        {
+            DefaultUser user = await _userManager.GetUserAsync(User);
+
+            // Kiểm tra xem người dùng có tồn tại không
+            if (user == null)
+            {
+                // Xử lý lỗi, người dùng không được tìm thấy hoặc không đăng nhập
+                BadRequest("User not found or not logged in.");
+            }
+            IEnumerable<Order> ordered = await _context.Orders
+                .Where(o => o.UserId == user.Id)
+                .ToListAsync();
+            if (ordered == null)
+            {
+                BadRequest("bạn không có đơn hàng nào cả");
+            }
+            return View(ordered);
+        }
+        public async Task<IActionResult> GetOrderItem(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var orderitem = await _context.Orders
+                .Include(b => b.OrderItems)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (orderitem == null)
+            {
+                return NotFound();
+            }
+
+            return View(orderitem);
+        }
+        //[HttpGet]
+        //public async Task<IActionResult> GetOrderItem()
+        //{
+        //    DefaultUser user = await _userManager.GetUserAsync(User);
+
+        //    // Kiểm tra xem người dùng có tồn tại không
+        //    if (user == null)
+        //    {
+        //        // Xử lý lỗi, người dùng không được tìm thấy hoặc không đăng nhập
+        //        BadRequest("User not found or not logged in.");
+        //    }
+        //    IEnumerable<OrderItem> orderitem = await _context.Orders
+        //        .Where(o => o.UserId == user.Id)
+        //        .ToListAsync();
+        //    if (ordered == null)
+        //    {
+        //        BadRequest("bạn không có đơn hàng nào cả");
+        //    }
+        //    return View(ordered);
+        //}
+        public IActionResult GetOrder(Order order)
         {
             return View();
         }
